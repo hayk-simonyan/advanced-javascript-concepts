e65ea8a4933262535a75d9de8011d5aa0e8e04d6
Hayk <hsimonyan09@gmail.com>
2019-06-16 18:49:33 +0400

    Types in JavaScript

---
+++ 3. Types in JavaScript/02.pass-by-reference.js
@@ -0,0 +1,51 @@
+var c = [1,2,3];
+var d = c;
+d.push( 4 );
+
+console.log(c);   // [1,2,3,4]
+console.log(d);   // [1,2,3,4]
+
+var a = 5;
+var b = a;
+
+b++;
+
+let obj = {
+  a: 'a',
+  b: 'b',
+  c: {
+    deep: 'try and copy me'
+  }
+};
+let clone = Object.assign({}, obj);
+let clone2 = {...obj}
+let superClone = JSON.parse(JSON.stringify(obj))
+
+obj.c.deep = 'hahaha';
+console.log(obj)
+console.log(clone)
+console.log(clone2)
+console.log(superClone)
+
+const number = 100
+const string = "Jay"
+let obj1 = {
+  value: "a"
+}
+let obj2 = {
+  value: "b"
+}
+let obj3 = obj2;
+ 
+function change(number, string, obj1, obj2) {
+    number = number * 10;
+    string = "Pete";
+    obj1 = obj2;
+    obj2.value = "c";
+}
+ 
+change(number, string, obj1, obj2);
+ 
+console.log(number); 
+console.log(string);
+console.log(obj1.value);
